33fa86ad550958a818e593fd909b99e88cfba94a
Worse2Worst <wasuwach@hotmail.com>
2022-03-01 18:52:31 +0900

    Remove unnecessary parenthesis

--- roman_converter.py
+++ roman_converter.py
@@ -8,5 +8,5 @@ def integer_to_roman(num):
     hundreds = c[(num % 1000) // 100]
     tens = x[(num % 100) // 10]
     ones = i[num % 10]
-    res = (thousands + hundreds + tens + ones)
+    res = thousands + hundreds + tens + ones
     return res
